b26d196ffeda44f776992dde3007565d03db621b
hexagrimoire <hexagrimoire@gmail.com>
2021-10-04 14:38:54 +0900

    githubから環境作るように修正

--- auth/files/run.sh
+++ auth/files/run.sh
@@ -3,6 +3,15 @@
 if [ ! -d "/tmp/check" ]; then
 
 mkdir /tmp/check
+  pushd /opt/perforce
+  wget https://github.com/perforce/helix-authentication-service/archive/refs/tags/2020.2.zip
+  unzip 2020.2.zip
+  rm -Rf /opt/perforce/2020.2.zip
+  chown -R perforce:perforce /opt/perforce/helix-authentication-service-2020.2
+  mv /opt/perforce/helix-authentication-service-2020.2 /opt/perforce/helix-auth-svc
+  pushd /opt/perforce/helix-auth-svc
+  sudo -E -u perforce bash install.sh -n
+  popd
   echo `date` > /tmp/check/first_`date +%Y%m%d_%H%M%S`
 
 else
